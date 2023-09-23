e17ed2ddb9ceb822b48ea9dc66e908ac73dad7cc
Rita Mordio <ritamordio0716@gmail.com>
2023-09-23 20:26:44 +0900

    none

---
+++ chapter01/1.4/statement.js
@@ -0,0 +1,85 @@
+import invoices from '../invoices.json' assert { type: 'json' }
+import plays from '../plays.json' assert { type: 'json' }
+export function statement(invoice) {
+  let result = `청구내역 (고객명: ${invoice.customer})\n`
+
+  for (let perf of invoice.performances) {
+    // 청구 내역을 출력한다.
+    result += `${playFor(perf).name}: ${usd(amountFor(perf))} ${perf.audience}석\n`
+  }
+
+  result += `총액 ${usd(totalAmount(invoice))}\n`
+  result += `적립 포인트 ${totalVolumeCredits(invoice)}점\n`
+
+  return result
+
+  function totalAmount() {
+    let result = 0
+    for (let perf of invoice.performances) {
+      result += amountFor(perf)
+    }
+    return result
+  }
+
+  function totalVolumeCredits() {
+    let result = 0
+    for (let perf of invoice.performances) {
+      // 포인트를 적립한다.
+      result += volumeCreditsFor(perf)
+    }
+
+    return result
+  }
+
+  function usd(aNumber) {
+    return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', minimumFractionDigits: 2 }).format(
+      aNumber / 100,
+    )
+  }
+
+  function volumeCreditsFor(perf) {
+    let result = 0
+
+    result += Math.max(perf.audience - 30, 0)
+
+    // 희극 관객 5명마다 추가 포인트를 제공한다.
+    if ('comedy' === playFor(perf).type) {
+      result += Math.floor(perf.audience / 5)
+    }
+
+    return result
+  }
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID]
+  }
+
+  function amountFor(aPerformance) {
+    let result = 0
+
+    switch (playFor(aPerformance).type) {
+      case 'tragedy':
+        result = 40_000
+
+        if (aPerformance.audience > 30) {
+          result += 1_000 * (aPerformance.audience - 30)
+        }
+        break
+      case 'comedy':
+        result = 30_000
+
+        if (aPerformance.audience > 20) {
+          result += 10_000 + 500 * (aPerformance.audience - 20)
+        }
+        result += 300 * aPerformance.audience
+        break
+
+      default:
+        throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`)
+    }
+
+    return result
+  }
+}
+
+console.log(statement(invoices, plays))
